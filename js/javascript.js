4e93fa04b1eae2ffc71d301cf98ba932e3762039
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-16 14:36:30 +0200

    primo ES fatto

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,23 @@
-// eserzio EMAIL---------
+/*
+esercizio 1: L’utente inserisce due numeri in successione, con due prompt.
+  Il software stampa il maggiore.
+*/
 
-var valore1=parseInt(prompt('inserisci il valore 1'));
-var valore2=parseInt(prompt('inserisci il valore 2'));
-var valore3=parseInt(prompt('inserisci il valore 3'));
-var valore4=parseInt(prompt('inserisci il valore 4'));
-var valore5=parseInt(prompt('inserisci il valore 5'));
+var valore1 =parseInt(prompt('inserire il primo valore'));
+var valore2 =parseInt(prompt('inserire il secondo valore'));
+
+if(valore1>valore2){
+    console.log(valore1)
+    alert('il valore più alto '+ valore1);
+}
+else if(valore1<valore2){
+    console.log(valore2);
+    alert('il valore più alto '+ valore2);
+}
+else{
+    console.log('i valori sono identici');
+    alert('i valori sono identici.. cioè ' + valore1);
+}
 
 
 
