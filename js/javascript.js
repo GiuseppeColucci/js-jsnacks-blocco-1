786b792005589b2910587820f861d208e31676f9
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-16 14:17:03 +0200

    prova

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+// eserzio EMAIL---------
+
+var valore1=parseInt(prompt('inserisci il valore 1'));
+var valore2=parseInt(prompt('inserisci il valore 2'));
+var valore3=parseInt(prompt('inserisci il valore 3'));
+var valore4=parseInt(prompt('inserisci il valore 4'));
+var valore5=parseInt(prompt('inserisci il valore 5'));
+
+
+
+
+
+
+
+
+
+
+
